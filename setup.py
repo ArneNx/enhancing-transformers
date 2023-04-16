869bae061bd3225083efaace93c5c824b42ec747
Arne Nix <arnenix@googlemail.com>
2023-04-16 12:42:34 +0200

    new gitignore + pip installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="enhancing",
+    version="0.1dev",
+    description="",
+    author="",
+    author_email="",
+    packages=["enhancing"],  # same as name
+    install_requires=[],  # external packages as dependencies
+)
